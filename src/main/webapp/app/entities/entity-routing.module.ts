80df619ad3eafbce89c86fc2336d87b900acb473
Ash <aaalarayed@gmail.com>
2021-12-03 04:59:25 +0200

    new project

--- src/main/webapp/app/entities/entity-routing.module.ts
+++ src/main/webapp/app/entities/entity-routing.module.ts
@@ -4,6 +4,31 @@ import { RouterModule } from '@angular/router';
 @NgModule({
   imports: [
     RouterModule.forChild([
+      {
+        path: 'supplier',
+        data: { pageTitle: 'Suppliers' },
+        loadChildren: () => import('./supplier/supplier.module').then(m => m.SupplierModule),
+      },
+      {
+        path: 'dispatch',
+        data: { pageTitle: 'Dispatches' },
+        loadChildren: () => import('./dispatch/dispatch.module').then(m => m.DispatchModule),
+      },
+      {
+        path: 'failure',
+        data: { pageTitle: 'Failures' },
+        loadChildren: () => import('./failure/failure.module').then(m => m.FailureModule),
+      },
+      {
+        path: 'car',
+        data: { pageTitle: 'Cars' },
+        loadChildren: () => import('./car/car.module').then(m => m.CarModule),
+      },
+      {
+        path: 'employee',
+        data: { pageTitle: 'Employees' },
+        loadChildren: () => import('./employee/employee.module').then(m => m.EmployeeModule),
+      },
       /* jhipster-needle-add-entity-route - JHipster will add entity modules routes here */
     ]),
   ],
